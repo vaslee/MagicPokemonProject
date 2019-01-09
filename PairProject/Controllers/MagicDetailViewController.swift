c51b42692e2f148be97e81086c2ee20eeb560f23
vaslee <ltx0503@gmail.com>
2019-01-09 17:36:16 -0500

    hi

--- PairProject/Controllers/MagicDetailViewController.swift
+++ PairProject/Controllers/MagicDetailViewController.swift
@@ -13,8 +13,7 @@ class MagicDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+print("hi")    }
     
 
     /*
